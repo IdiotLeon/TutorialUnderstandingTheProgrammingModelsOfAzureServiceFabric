eb69a9b4e438530bb2e5fe113ab2818eb65e45f0
Leon Lu <yanglu@microsoft.com>
2018-05-16 12:20:42 -0700

    demo: Product Catalog Service
    All previous data are saved without being removed when I close the service fabric project
    
    Module 4.4

---
+++ ECommerce.ProductCatalog/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ECommerce.ProductCatalog.Model;
+using System.Threading.Tasks;
+
+namespace ECommerce.ProductCatalog
+{
+    interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task AddProduct(Product product);
+    }
+}
---
+++ ECommerce.ProductCatalog/ServiceFabricProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using System.Threading;
+
+namespace ECommerce.ProductCatalog
+{
+    class ServiceFabricProductRepository : IProductRepository
+    {
+        private readonly IReliableStateManager _stateManager;
+        public ServiceFabricProductRepository(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+        public async Task AddProduct(Product product)
+        {
+            var products = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Product>>("product");
+
+            using (var transaction = _stateManager.CreateTransaction())
+            {
+                await products.AddOrUpdateAsync(transaction, product.Id, product, (id, value) => product);
+
+                await transaction.CommitAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            var products = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Product>>("product");
+            var result = new List<Product>();
+
+            using (var transaction = _stateManager.CreateTransaction())
+            {
+                var allProducts = await products.CreateEnumerableAsync(transaction, EnumerationMode.Unordered);
+
+                using (var enumerator = allProducts.GetAsyncEnumerator())
+                {
+                    while (await enumerator.MoveNextAsync(CancellationToken.None))
+                    {
+                        KeyValuePair<Guid, Product> current = enumerator.Current;
+                        result.Add(current.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
