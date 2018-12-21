7670df0ca80ebcca34128260ab736d857a85bc16
Leon <yanglyu.leon.7@gmail.com>
2018-12-20 16:41:49 -0800

    demo: Creating Web API

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.API.Model;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<ApiProduct>> Get()
+        {
+            return new[] { new ApiProduct() { Id = Guid.NewGuid(), Description = "fake" } };
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] ApiProduct product) {
+
+        }
+    }
+}
